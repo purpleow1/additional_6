b43b9e45e8610605decb9623a18f26d5ea664212
purpleow1 <purpleowllight@gmail.com>
2017-09-17 20:58:18 +0300

    zeros/feature: implement function zeros()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function zeros(expression) {
-  // your solution
+	var arrayString = expression.split("*");
+	var numberString;
+	var number;
+	var numberTime;
+	var countTwo = 0;
+	var countFive = 0;
+
+	for (var i = 0; i < arrayString.length; i++) {
+		numberString = arrayString[i];
+		number = parseInt(numberString);
+
+		//ordinary factorial
+		if (numberString[numberString.length-2] != "!") {
+			for (var j = 1; j <= number; j++) {
+				numberTime = j;
+				while (!(numberTime % 2)) {
+					numberTime /= 2;
+					countTwo++;
+				}
+				while (!(numberTime % 5)) {
+					numberTime /= 5;
+					countFive++;
+				}
+			}
+		}
+
+		//double factorial - odd
+		else if (number % 2) {
+			for (var j = 1; j <= number; j += 2) {
+				numberTime = j;
+				while (!(numberTime % 5)) {
+					numberTime /= 5;
+					countFive++;
+				}
+			}
+		}
+
+		//double factorial - even
+		else {
+			for (var j = 2; j <= number; j += 2) {
+				numberTime = j;
+				while (!(numberTime % 2)) {
+					numberTime /= 2;
+					countTwo++;
+				}
+				while (!(numberTime % 5)) {
+					numberTime /= 5;
+					countFive++;
+				}
+			}
+		}
+	}
+
+	return Math.min(countTwo, countFive);
 }
